6198e94e34017a4fe2f17e9c464ceed8c970cb3e
Alex <1571478933@qq.com>
2025-09-06 16:23:09 +0800

    Initial commit

---
+++ app/src/main/java/com/wkq/ad/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.wkq.ad.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wkq.ad.R
+
+/**
+ *
+ *@Author: wkq
+ *
+ *@Time: 2025/9/6 16:20
+ *
+ *@Desc:
+ */
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
